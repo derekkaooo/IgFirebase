7fba09bde92f8dfb3999950ed2726e4a204c1cc8
Derek <stiau334@gmail.com>
2019-03-05 23:18:34 +0800

    first commit

--- IgFirebase/Camera/CameraController.swift
+++ IgFirebase/Camera/CameraController.swift
@@ -60,6 +60,7 @@ class CameraController: UIViewController,AVCapturePhotoCaptureDelegate {
         let previewImageView = UIImageView(image: previewImage)
         view.addSubview(previewImageView)
         previewImageView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
     }
      let output = AVCapturePhotoOutput()
     
